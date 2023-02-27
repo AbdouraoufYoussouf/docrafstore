d215a5243c507374457062b0b8f0503c5c156312
Abdouraouf <youssouf.abdouraouf4@gmail.com>
2023-02-27 10:05:38 +0100

    smart

--- pages/smartphone/index.js
+++ pages/smartphone/index.js
@@ -1,16 +1,7 @@
 import React, { useState } from 'react'
 
 const index = () => {
-    const [array, setArray] = useState([1, 2, 3, 2, 4, 5]);
-
-    const newArray = array.filter((item) => item !== 2);
-
-    // Mettre à jour l'état du tableau avec le nouveau tableau
-    console.log(array)
-    console.log(newArray)
-  
-    // Mettre à jour l'état du tableau avec le nouveau tableau
-  return (
+    return (
     <div>index</div>
   )
 }
